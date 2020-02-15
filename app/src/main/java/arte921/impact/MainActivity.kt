696859b3cb3c5fd895c4fa06b430ddee64d0e8aa
arte921 <ardjanve@gmail.com>
2020-02-15 16:11:07 +0100

    lol

--- app/src/main/java/arte921/impact/MainActivity.kt
+++ app/src/main/java/arte921/impact/MainActivity.kt
@@ -1,17 +1,49 @@
 package arte921.impact
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private var gsensor: Sensor? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tva = findViewById<TextView>(R.id.tva)
-        tva.text = "howdy"
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        //tva.text = "howdy"
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent){
+        tva.text = event.values[0].toString()
+    }
+
+    override fun onResume(){
+        super.onResume()
+        gsensor?.also{gs ->
+            sensorManager.registerListener(this,gs,SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 
 }
