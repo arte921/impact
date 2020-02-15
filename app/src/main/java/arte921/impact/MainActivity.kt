2793520b894c703174a8c77d2c229d0f55b0b69f
arte921 <ardjanve@gmail.com>
2020-02-15 15:56:58 +0100

    lol

--- app/src/main/java/arte921/impact/MainActivity.kt
+++ app/src/main/java/arte921/impact/MainActivity.kt
@@ -2,11 +2,16 @@ package arte921.impact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tva = findViewById<TextView>(R.id.tva)
+        tva.text = "howdy"
     }
+
 }
