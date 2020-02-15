e5357e782d7c08fab9c3729258693205fb09bc34
arte921 <ardjanve@gmail.com>
2020-02-15 16:22:22 +0100

    lol

--- app/src/main/java/arte921/impact/MainActivity.kt
+++ app/src/main/java/arte921/impact/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
     private var gsensor: Sensor? = null
-
+    private var fres: Float = 0.0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +33,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent){
-        tva.text = event.values[0].toString()
+        fres = sqrt(event.values[0].pow(2)+event.values[1].pow(2)+event.values[2].pow(2))
+        tva.text = fres.toString()
     }
 
     override fun onResume(){
